3fbfe1ac10a5950353207fc458401b58c00c0fd3
Wiktor Mendalka <wiktor.mendalka@outlook.com>
2024-02-25 15:35:33 +0100

    remove nuke-repo script

--- nuke-repo.ps1
+++
@@ -1,9 +0,0 @@
-Remove-Item -Path .git -Force -Recurse  
-$commit_name = "Scheduled maintenance $(get-date -f yyyy-MM-dd)"
-
-git init
-git add --all
-git commit -m $commit_name
-
-git remote add origin https://github.com/Wiciaki/wiciaki.github.io
-git push -u --force origin main
